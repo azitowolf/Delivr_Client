ac0a6f0012abb75fdbca9213fbc5bfcce4bade24
azitowolf <azito.wolf3@gmail.com>
2015-07-14 12:18:06 -0400

    created front end model for Delivery creation form

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -3,8 +3,23 @@
 (function registerControllerIIFE(ang) {
 
   var UserCtrl = function() {
-    this.hideModal = true;
+    this.createDeliveryForm = {};
 
+    this.createDeliveryForm.user = 'currentUser.Id;';
+
+    this.createDeliveryForm.locationA = {};
+    this.createDeliveryForm.locationA.street = '';
+    this.createDeliveryForm.locationA.city = '';
+    this.createDeliveryForm.locationA.state = '';
+    this.createDeliveryForm.locationA.zipCode = '';
+
+    this.createDeliveryForm.description = '';
+
+    this.createDeliveryForm.locationB = {};
+    this.createDeliveryForm.locationB.street = '';
+    this.createDeliveryForm.locationB.city = '';
+    this.createDeliveryForm.locationB.state = '';
+    this.createDeliveryForm.locationB.zipCode = '';
 
   };
 
